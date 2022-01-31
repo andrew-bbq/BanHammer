49e8dc2dc49bbe8fa7c98e84b36f521bc25c8a61
Andrew Bennett <awbennett@wisc.edu>
2022-01-31 17:55:48 -0600

    A little bit of progress

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require('discord.js'); //import discord.js
+const { joinVoiceChannel } = require('@discordjs/voice');
 
-const client = new Discord.Client(); //create new client
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_BANS"] })
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+let connection;
+let online = false;
+let currentChannel;
+
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
+  if (msg.content === '!come') {
+    msg.reply('Coming!');
+    const member = msg.member;
+    const vc = member.voice.channel;
+    connection = joinVoiceChannel({
+      channelId: vc.id,
+      guildId: vc.guild.id,
+      adapterCreator: vc.guild.voiceAdapterCreator,
+    });
+    online = true;
+    currentChannel = vc;
+
+    connection.on('speaking', (user, speaking) => {
+      console.log("WHAT UP BOYS");
+    });
+  }
+
+  if (msg.content === '!uncome') {
+    if(connection != null) {
+      connection.destroy();
+      online = false;
     }
-  });
+  }
+});
+
+client.on("guildMemberSpeaking", (member, bool) => {
+  console.log(member);
+  console.log(bool);
+});
+
 
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
